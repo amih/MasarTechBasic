004aedf376ba0b4c70c32af2b83911be9dc6de30
Ami Heines <ami.heines@gmail.com>
2022-05-29 16:55:23 +0300

    updated lesson 3

--- lesson3/index.js
+++ lesson3/index.js
@@ -109,20 +109,21 @@ makeNewPersonObject = (theNewName, theAge, shoeSize) => {
 person = { firstName: "Ami", lastName: "Heines", age: 51, shoeSize: 45 }
 
 
-getOnlyEvenNumbers = (theListOfNumbers) => {
-	console.log('start the length of the parameter:' + theListOfNumbers.lenght)
+getOnlyEvenNumbers = (param) => {
+	console.log('good:' + param.length)
+	console.log('bad :' + param.lenght)
   s = []
-  for(i=0; i<theListOfNumbers.lenght; i++){
-		console.log('in loop, i:' + i)
-		if( theListOfNumbers[i] % 2 == 0 ){
-			console.log('found even num:' + theListOfNumbers[i])
-			s.push( theListOfNumbers[i] )
+  for(i=0; i<param.lenght; i++){
+		// console.log('in loop, i:' + i)
+		if( param[i] % 2 == 0 ){
+			// console.log('found even num:' + param[i])
+			s.push( param[i] )
 		}
 	}
 	return s
 }
 console.log('testing get EVEN numbers:')
-myArray = [ 432,234,23,4,234,2,34,4,54,6,4,67,5,8,854,6,456 ]
+myArray = [ 432,234,23,4,234,1,2,2,2,2,3,5,2,34,4,54,6,4,67,5,8,854,6,456,3,999 ]
 console.log( getOnlyEvenNumbers(myArray) )
 console.log('testing AGAIN get EVEN numbers:')
 myList = [ 1,2,3,4,5,6,7,8,9,10  ]
@@ -130,5 +131,17 @@ console.log( getOnlyEvenNumbers(myList) )
 
 // 1. write a function "getOnlyEvenNumbers" which gets an input a list of numbers
 //    and returns only the even numbers ( the ones that have num % 2 == 0 )
-
-// 2. 
+//
+//    the function is almost ready, fix the bug!
+//    hint: look carefully at the good and bad lines, 113 and 114.
+// 2. In your html File, add to the <head> a link tag with css property of './index.css'
+// 3. create a new file called index.css in your folder
+// 4. add css commands to it, similar to this project.
+// 		#explainerText{ ... }
+// 		#multiplicationTable{ ... }
+// 		.even{ ... }
+// 5. Change your html so it will have one div with the text above the multiplication table
+//    and another empty div for the multiplication table.
+// 6. change your multiplication table code in your js file, so it will be like the code in this file
+// 7. change the multiplication table to make odd lines style: background-color: yellow
+// 8. change the parameter from "% 2" to "% 3" and check what happens
