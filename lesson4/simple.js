9e3e1fb11829f7b682c6b923d6c65a465ddce653
Ami Heines <ami.heines@gmail.com>
2022-06-12 18:09:40 +0300

    started work on edit feature in simple todo list

--- lesson4/simple.js
+++ lesson4/simple.js
@@ -1,22 +1,53 @@
-// $('div').on('click', (e)=>{
-//     console.log(e.target.innerHTML
-// )})
-
-$('#addItemToList').on('click', ()=>{
+addNewItem = () => {
 	newValue = $('#newItemToAdd')[0].value
-  // console.log('user clicked on "#addItemToList", the new item is: ' + newValue)
-	$('ul').append('<li>' + newValue + ' <button class="deleteMe">X</button> </li>')
-})
-
-$('#newItemToAdd').on('keydown', ( e )=>{
-	// console.log('user pressedKey, the actual key is::::', e.originalEvent.key)
-	if( e.originalEvent.key == 'Enter' ){
-		newValue = $('#newItemToAdd')[0].value
-		$('ul').append('<li>' + newValue + ' <button class="deleteMe">X</button> </li>')
+	if(newValue != ''){
+		$('ul').append('<li><div class="itemName">' + newValue + ' </div> <button class="deleteMe">X</button> </li>')
+		$('#newItemToAdd')[0].value = ''
 	}
+}
+
+$(()=>{
+	$('#addItemToList').on('click', ()=>{
+		addNewItem()
+	})
+	
+	$('#newItemToAdd').on('keydown', ( e )=>{
+		if( e.originalEvent.key == 'Enter' ){
+			addNewItem()
+		}
+	})
+	
+	$('ul').on('click', '.deleteMe', (e)=>{
+		$(e.target).parent().remove()
+	})
+	
+	$('.itemName').on('click', ( e ) => {
+		// if user clicks the item, allow the user to edit it!
+		oldValue = $( e.target )[0].innerHTML
+		$( e.target ).parent().prepend('<input class="editOldValue" type="text" value="' + oldValue + '"></input>')
+		// hide the div class itemName
+		$( e.target ).hide()
+		// <li>
+		//PREPEND --> <input class="editOldValue" type="text" value="..."></input>
+		//   <div class="itemName">
+		// 	   Clean room
+		// 	 </div>
+		// 	 <button class="deleteMe">
+		// 	   X
+		//   </button>
+		// </li>
+	})
+	$('ul').on('keydown', '.editOldValue', ( e ) => {
+		if( e.originalEvent.key == 'Enter' ){
+			editedItem = $(e.target)[0].value
+			console.log('new edited value: ', editedItem)
+			// put new value in div class itemName
+			$( e.target ).parent().find('.itemName')[0].innerHTML = editedItem
+			// show .itemName
+			$( e.target ).parent().find('.itemName').show()
+			// delete me!
+			$( e.target ).remove()
+		}
+	})
 })
 
-$('ul').on('click', '.deleteMe', (e)=>{
-  console.log('clicked on deleteMe!', e.target)
-	$(e.target).parent().remove()
-})
